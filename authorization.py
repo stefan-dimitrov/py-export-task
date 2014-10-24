b9b7ecaae24a2fe67c743d812fcaea8d0936ba19
stefan.dimitrov <stefan.dimitrov.009@gmail.com>
2014-10-24 14:37:57 +0300

    Add(gcsDownload.py). Modify(authorization.py): add authorizeGCS function.

--- authorization.py
+++ authorization.py
@@ -28,7 +28,35 @@ def authorizeBQ(secretJsonFile):
     # If the credentials don't exist or are invalid, run the native client
     # auth flow. The Storage object will ensure that if successful the good
     # credentials will get written back to a file.
-    storage = Storage('bigquery2.dat')  # Choose a file name to store the credentials.
+    storageFile = 'bigquery2.dat'
+
+    http = _authorizeCredentials(FLOW, storageFile)
+    return http
+
+
+def authorizeGCS(secretJsonFile):
+    """
+    :type secretJsonFile: basestring
+    :rtype : httplib2.Http
+    """
+    FLOW = flow_from_clientsecrets(secretJsonFile, 'https://www.googleapis.com/auth/devstorage.read_only')
+
+    # If the credentials don't exist or are invalid, run the native client
+    # auth flow. The Storage object will ensure that if successful the good
+    # credentials will get written back to a file.
+    storageFile = 'gcs.dat'
+
+    http = _authorizeCredentials(FLOW, storageFile)
+    return http
+
+
+def _authorizeCredentials(FLOW, storageFile):
+    """
+    :type FLOW: OAuth2WebServerFlow
+    :type storageFile: basestring
+    :rtype : httplib2.Http
+    """
+    storage = Storage(storageFile)  # Choose a file name to store the credentials.
     credentials = storage.get()
     if credentials is None or credentials.invalid:
         credentials = run(FLOW, storage)
---
+++ gcsDownload.py
@@ -0,0 +1,75 @@
+import httplib2
+import os
+import random
+import sys
+import time
+
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+
+
+# Retry transport and file IO errors.
+RETRYABLE_ERRORS = (httplib2.HttpLib2Error, IOError)
+
+# Number of times to retry failed downloads.
+NUM_RETRIES = 5
+
+# Number of bytes to send/receive in each request.
+CHUNKSIZE = 2 * 1024 * 1024
+
+# Mimetype to use if one can't be guessed from the file extension.
+DEFAULT_MIMETYPE = 'application/octet-stream'
+
+
+def print_with_carriage_return(s):
+    sys.stdout.write('\r' + s)
+    sys.stdout.flush()
+
+
+def download(service, bucketName, objectName, filename):
+
+    print 'Building download request...'
+    f = file(filename, 'w')
+    request = service.objects().get_media(bucket=bucketName,
+                                          object=objectName)
+    media = MediaIoBaseDownload(f, request, chunksize=CHUNKSIZE)
+
+    print 'Downloading bucket: %s object: %s to file: %s' % (bucketName,
+                                                             objectName,
+                                                             filename)
+
+    progressless_iters = 0
+    done = False
+    while not done:
+        error = None
+        try:
+            progress, done = media.next_chunk()
+            if progress:
+                print_with_carriage_return(
+                    'Download %d%%.' % int(progress.progress() * 100))
+        except HttpError, err:
+            error = err
+            if err.resp.status < 500:
+                raise
+        except RETRYABLE_ERRORS, err:
+            error = err
+
+        if error:
+            progressless_iters += 1
+            handle_progressless_iter(error, progressless_iters)
+        else:
+            progressless_iters = 0
+
+    print '\nDownload complete!'
+
+
+def handle_progressless_iter(error, progressless_iters):
+    if progressless_iters > NUM_RETRIES:
+        print 'Failed to make progress for too many consecutive iterations.'
+        raise error
+
+    sleeptime = random.random() * (2 ** progressless_iters)
+    print ('Caught exception (%s). Sleeping for %s seconds before retry #%d.'
+           % (str(error), sleeptime, progressless_iters))
+    time.sleep(sleeptime)
